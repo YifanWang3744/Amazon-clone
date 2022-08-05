7474540130a429f745d40cb3c4b540217650da03
frankwang9899 <dtmd2017>
2022-08-05 17:08:44 -0400

    fetching products

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import products from './data/products.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
