6ea36bb17c2cbc59b4ace9f63c7c6fc1b6b25b2e
frankwang9899 <dtmd2017>
2022-08-05 00:38:58 -0400

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
