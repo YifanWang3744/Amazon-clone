b9872749ab9ba9db4c1955a6f046a4e4993e2955
frankwang9899 <dtmd2017>
2022-08-11 16:25:36 -0400

    mongodb setup + redux

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 app.use(cors());
@@ -11,6 +17,12 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.get('/api/products', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -27,5 +39,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
